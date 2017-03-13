74a990ecab09d4e440e8773dd178097392427355
Maddie Gabriel <mgabri02@mail.uoguelph.ca>
2017-03-13 11:02:50 -0400

    initial commit

---
+++ js/calculator.js
@@ -0,0 +1,21 @@
+function buttonPlus() {
+    document.Calculator.output.value += "+"; 
+}
+function buttonMinus() {
+    document.Calculator.output.value += "-";
+}
+function buttonMult() {
+    document.Calculator.output.value += "*";
+}
+function buttonPercent() {
+    document.Calculator.output.value += "%";
+}
+function buttonDecimal() {
+    document.Calculator.output.value += ".";
+}
+function buttonDivide() {
+    document.Calculator.output.value += "/";
+}
+function buttonClear() {
+    document.Calculator.output.value = "";
+}
