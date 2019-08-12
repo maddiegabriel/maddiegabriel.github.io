0644bd91682f16d41e97ac5d374f2521f617ed77
mabbieg <mgabri02@mail.uoguelph.ca>
2019-08-11 22:50:51 -0400

    slider + next round of pots

--- test.java
+++ test.java
@@ -1,23 +1,3 @@
-
-
-
-Customer currentCustomer;
-
-btn.setOnAction(new EventHandler<ActionEvent>() {
-    @Override
-    public void handle(ActionEvent event) {
-        String username = user.getText();
-        String password = pass.getText();
-    }
-});
-
-public Array
-public void readFromFile() {
-
-}
-
-public Customer findCustomer(String lastName, String password) {
-    for(int i = 0; i < arr.size(); i++) {
-        
-    }
-}
+for(int i=0; i < changeFile.size(); i++) {
+    System.out.println(changeFile.get(i).toString());
+} 
