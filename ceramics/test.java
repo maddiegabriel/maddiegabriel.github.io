dc72d86d7b94778a926d1d4a7a063b46d88eac77
mabbieg <mgabri02@mail.uoguelph.ca>
2019-08-14 15:51:48 -0400

    added IMGD page, about page. portfolio almost finished

---
+++ ceramics/test.java
@@ -0,0 +1,9 @@
+ObservableList<Employee> employees = FXCollections.observableArrayList
+for() {
+    
+}
+
+ObservableList<StudentsModel> studentsModels = FXCollections.observableArrayList(
+        new StudentsModel(1,"Amos", "Chepchieng"),
+        new StudentsModel(2,"Keep", "Too"),
+);
